6faf234f5853f5ea8374b4d05b3dfc8ca65258c9
kasajuBivek <bkasaju97@gmail.com>
2024-07-23 12:49:10 -0400

    resp

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import './Navbar.css';
 import logo from '../../assets/lauraJoyLogo.png';
 import { HashLink } from 'react-router-hash-link';
-import { Link, useLocation  } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -11,21 +11,28 @@ const Navbar = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
+  const handleLinkClick = () => {
+    setIsMenuOpen(false);
+  };
+
   const location = useLocation();
   const isInPractiseAreasOrMedia = location.pathname.includes('/practiseAreas') || location.pathname.includes('/media');
+  
   return (
     <nav className='container'>
-      {<HashLink to={isInPractiseAreasOrMedia ? "/#hero" : "#hero"}><img src={logo} alt="Logo" className='logo' /></HashLink>}
+      <HashLink to={isInPractiseAreasOrMedia ? "/#hero" : "#hero"} onClick={handleLinkClick}>
+        <img src={logo} alt="Logo" className='logo' />
+      </HashLink>
       <button className='menu-toggle' onClick={toggleMenu}>
         {isMenuOpen ? 'Close' : 'Menu'}
       </button>
       <ul className={isMenuOpen ? 'active' : ''}>
-        <li><HashLink to={isInPractiseAreasOrMedia ? "/#hero" : "#hero"}>Home</HashLink></li>
-        <li><HashLink to={isInPractiseAreasOrMedia ? "/#about" : "#about"}>About</HashLink></li>
-        <li><HashLink to={isInPractiseAreasOrMedia ? "/#practiseAreas" : "#practiseAreas"}>Practise Areas</HashLink></li>
-        <li><HashLink to={isInPractiseAreasOrMedia ? "/#media" : "#media"}>Media</HashLink></li>
-        <li><HashLink to={isInPractiseAreasOrMedia ? "/#testimonials" : "#testimonials"}>Testimonials</HashLink></li>
-        <li><button className='btn'><HashLink to="#contactUs">Contact us</HashLink></button></li>
+        <li><HashLink to={isInPractiseAreasOrMedia ? "/#hero" : "#hero"} onClick={handleLinkClick}>Home</HashLink></li>
+        <li><HashLink to={isInPractiseAreasOrMedia ? "/#about" : "#about"} onClick={handleLinkClick}>About</HashLink></li>
+        <li><HashLink to={isInPractiseAreasOrMedia ? "/#practiseAreas" : "#practiseAreas"} onClick={handleLinkClick}>Practise Areas</HashLink></li>
+        <li><HashLink to={isInPractiseAreasOrMedia ? "/#media" : "#media"} onClick={handleLinkClick}>Media</HashLink></li>
+        <li><HashLink to={isInPractiseAreasOrMedia ? "/#testimonials" : "#testimonials"} onClick={handleLinkClick}>Testimonials</HashLink></li>
+        <li><button className='btn'><HashLink to="#contactUs" onClick={handleLinkClick}>Contact us</HashLink></button></li>
       </ul>
     </nav>
   );
